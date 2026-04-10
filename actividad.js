20ef4cccad360192e162ee7eb810ef3ddd1f63fc
Maligno <c.lancellotti1@alumnos.santotomas.cl>
2026-04-10 10:38:05 -0400

    Add files via upload

---
+++ actividad.js
@@ -0,0 +1,9 @@
+// 1.ALERTA EMERGENTE
+alert("Hola Mundo desde una alerta");
+
+//2.MENSAJE DE LA PAGINA Y MESANJE DENTRO DEL DIV
+document.getElementById("mensajePagina").innerText = "Hola Mundo en la página";
+document.getElementById("contenedor").innerHTML = "Hola mundo dentro del div";
+
+//3.SALIDA X CONSOLA
+console.log("Hola Mundo en la consola");
